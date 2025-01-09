0ba568402a72b483824d9b4e6b8c6ee74891a93e
olearyc11 <157394769+olearyc11@users.noreply.github.com>
2025-01-09 10:55:53 -0500

    Update

--- core/src/test/java/com/funnelsensai/core/CalendarEventsTest.java
+++ core/src/test/java/com/funnelsensai/core/CalendarEventsTest.java
@@ -1,10 +1,8 @@
 package com.funnelsensai.core;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.funnelsensai.core.dto.CalendarEvent;
-import com.funnelsensai.core.dto.CalendarEventsResponse;
 import com.funnelsensai.core.service.CalendarEventService;
 import okhttp3.*;
 import org.junit.jupiter.api.AfterEach;
