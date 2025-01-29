1bc6a5393a385999cc69355a531d0dc62d897200
Alex-Cultrera <alexander.cultrera@gmail.com>
2025-01-29 10:50:03 -0500

    updated dto/opportunities/CustomField and dto/opportunnities/util/FieldValueDeserializer

--- core/src/main/java/com/funnelsensai/core/dto/opportunities/util/FieldValueDeserializer.java
+++ core/src/main/java/com/funnelsensai/core/dto/opportunities/util/FieldValueDeserializer.java
@@ -11,6 +11,21 @@ public class FieldValueDeserializer extends JsonDeserializer<Object> {
 
     @Override
     public Object deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-        return null;
+        switch (p.getCurrentToken()) {
+            case VALUE_STRING:
+                return p.getText();
+            case VALUE_NUMBER_INT:
+                return p.getLongValue();
+            case VALUE_NUMBER_FLOAT:
+                return p.getDoubleValue();
+            case VALUE_TRUE:
+            case VALUE_FALSE:
+                return p.getBooleanValue();
+            case VALUE_NULL:
+                return null;
+            default:
+                // For complex objects or arrays, read them as a generic Object
+                return p.readValueAs(Object.class);
+        }
     }
 }
